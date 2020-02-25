279c1d5b5fefca0d84a86749a7231621f6f30675
weversonfvieira <weversonvieira19@gmail.com>
2020-02-24 21:10:19 -0300

    Retirada as cumprimentações!

--- src/programa/weverson/Main.java
+++ src/programa/weverson/Main.java
@@ -3,11 +3,6 @@ package programa.weverson;
 public class Main {
 	public static void main(String[] args) {
 		
-		
-		System.out.println("Bom dia");
-		System.out.println("Boa tarde");
-		System.out.println("Boa noite");
-		
 		System.out.println("Helloworld");
 	}
 }
