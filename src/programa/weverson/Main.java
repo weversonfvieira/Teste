0cbf393319f2d7b61ecc854dd420f7b60912fe43
weversonfvieira <weversonvieira19@gmail.com>
2020-02-24 21:06:13 -0300

    Projeto criado

---
+++ src/programa/weverson/Main.java
@@ -0,0 +1,13 @@
+package programa.weverson;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Bom dia");
+		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
+		
+		System.out.println("Helloworld");
+	}
+}
